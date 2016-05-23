6611ef9f5af1380110829bb634320883e1d83b2f
FallenKN <fallen.kn@gmail.com>
2016-05-23 23:13:02 +0200

    Call me Leonadro the retardo

--- pypy.cmd
+++ pypy.cmd
@@ -3,10 +3,10 @@ Title PyPy-Batch
 goto ie
 
 :ie
-IF EXIST "%CD%/pypy-5.1.1-win32/pypy.exe" (goto f) else goto nf
+IF EXIST "%CD%\pypy-5.1.1-win32\pypy.exe" (goto f) else goto nf
 
 :f
-set PYPY="%CD%/pypy-5.1.1-win32/pypy.exe"
+set PYPY="%CD%\pypy-5.1.1-win32\pypy.exe"
 set /p PYFILE= Type in the name of your python script: 
 call %PYPY% %PYFILE%
 
@@ -16,17 +16,17 @@ set /p CHOICE= Do you want to download PyPy 5.1.1? y/n:
 IF /I "%CHOICE%"=="y" (goto dl) else goto e
 
 :dl
-bitsadmin.exe /TRANSFER "Downloading wget, please wait..." http://fallenkn.cf/files/wget.exe "%CD%/wget.exe"
+bitsadmin.exe /TRANSFER "Downloading wget, please wait..." http://fallenkn.cf/files/wget.exe "%CD%\wget.exe"
 cls
 call wget.exe http://fallenkn.cf/files/7za.exe
 cls
 call wget.exe https://bitbucket.org/pypy/pypy/downloads/pypy-5.1.1-win32.zip
 cls
-call 7za.exe x -y "%CD%/pypy-5.1.1-win32.zip" -o"%CD%/"
+call 7za.exe x -y "%CD%\pypy-5.1.1-win32.zip" -o"%CD%\"
 cls
-del /Q "%CD%/wget.exe"
-del /Q "%CD%/7za.exe"
-del /Q "%CD%/pypy-5.1.1-win32.zip"
+del /Q "%CD%\wget.exe"
+del /Q "%CD%\7za.exe"
+del /Q "%CD%\pypy-5.1.1-win32.zip"
 cls
 goto ie
 
