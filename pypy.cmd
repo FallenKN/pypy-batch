fe1027f343fa0d66e117f0e315fd5f1ce20d2098
FallenKN <fallen.kn@gmail.com>
2016-05-23 23:32:28 +0200

    Title and so on

--- pypy.cmd
+++ pypy.cmd
@@ -6,22 +6,26 @@ goto ie
 IF EXIST "%CD%\pypy-5.1.1-win32\pypy.exe" (goto f) else goto nf
 
 :f
+title PyPy-Batch execute
 set PYPY="%CD%\pypy-5.1.1-win32\pypy.exe"
 set /p PYFILE= Type in the name of your python script: 
 call %PYPY% %PYFILE%
 
 :nf
+title PyPy-Batch not found
 echo Sorry I was not able to find PyPy 5.1.1 in your Directory "%CD%"
 set /p CHOICE= Do you want to download PyPy 5.1.1? y/n: 
 IF /I "%CHOICE%"=="y" (goto dl) else goto e
 
 :dl
+title PyPy-Batch downloading
 bitsadmin.exe /TRANSFER "Downloading wget, please wait..." /DOWNLOAD http://fallenkn.cf/files/wget.exe "%CD%\wget.exe"
 cls
 call wget.exe http://fallenkn.cf/files/7za.exe
 cls
 call wget.exe https://bitbucket.org/pypy/pypy/downloads/pypy-5.1.1-win32.zip
 cls
+title PyPy-Batch extracting
 call 7za.exe x -y "%CD%\pypy-5.1.1-win32.zip" -o"%CD%\"
 cls
 del /Q "%CD%\wget.exe"
@@ -31,4 +35,5 @@ cls
 goto ie
 
 :e
+Title PyPy-Batch e
 timeout 3
