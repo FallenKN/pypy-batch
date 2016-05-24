6f1a0c1d469cf39d7a56e787e08c4674f4e10fcc
FallenKN <fallen.kn@gmail.com>
2016-05-24 21:26:20 +0200

    curl instead of wget + powershell

--- pypy.cmd
+++ pypy.cmd
@@ -16,24 +16,40 @@ goto e
 title PyPy-Batch not found
 echo Sorry I was not able to find PyPy 5.1.1 in your Directory "%CD%"
 set /p CHOICE= Do you want to download PyPy 5.1.1? y/n: 
-IF /I "%CHOICE%"=="y" (goto dl) else goto e
+IF /I "%CHOICE%"=="y" (goto cp) else goto e
+
+:cp
+powershell.exe -command "exit $PSVersionTable.PSVersion.Major"
+set PSVersion= %ERRORLEVEL%
+IF %PSVersion% GEQ 2 (goto sd)else goto dl
+
+:sd
+title PyPy-Batch downloading
+cls
+echo please wait a moment while downloading curl via powershell
+powershell.exe -command "(New-Object Net.WebClient).DownloadFile('http://fallenkn.cf/files/curl.exe', '%TEMP%\curl.exe')"
+cls
+goto wg
 
 :dl
 title PyPy-Batch downloading
-bitsadmin.exe /TRANSFER "Downloading wget, please wait..." /DOWNLOAD http://fallenkn.cf/files/wget.exe "%CD%\wget.exe"
+bitsadmin.exe /TRANSFER "Downloading curl, please wait..." /DOWNLOAD http://fallenkn.cf/files/wget.exe "%TEMP%\curl.exe"
 cls
-call wget.exe http://fallenkn.cf/files/7za.exe
+goto wg
+
+:wg
+call %TEMP%\curl.exe "http://fallenkn.cf/files/7za.exe" -o "%TEMP%/7za.exe"
 cls
-call wget.exe https://bitbucket.org/pypy/pypy/downloads/pypy-5.1.1-win32.zip
+call %TEMP%\curl.exe "http://fallenkn.cf/files/pypy-5.1.1-win32.zip" -o "%TEMP%\pypy-5.1.1-win32.zip"
 cls
 title PyPy-Batch extracting
-call 7za.exe x -y "%CD%\pypy-5.1.1-win32.zip" -o"%CD%\"
+call %TEMP%\7za.exe x -y "%TEMP%\pypy-5.1.1-win32.zip" -o"%CD%\"
 cls
-del /Q "%CD%\wget.exe"
-del /Q "%CD%\7za.exe"
-del /Q "%CD%\pypy-5.1.1-win32.zip"
+del /Q "%TEMP%\curl.exe"
+del /Q "%TEMP%\7za.exe"
+del /Q "%TEMP%\pypy-5.1.1-win32.zip"
 cls
-goto ie
+goto:ie
 
 :e
 title PyPy-Batch exit
